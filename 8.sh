07c69f58dfaf55c11e9d42794ac5c09d9357eee2
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-10 23:38:30 +0530

    Update 8.sh

--- 8.sh
+++ 8.sh
@@ -14,3 +14,23 @@ dont want"
 read y
 done
 cat item.txt
+
+# Output:-
+# set y as 1
+# 1
+# enter the item:
+# Item1
+# enter the itemcode:
+# 01
+# enter 1 if you want to enter item or 0 if you 
+# dont want
+# 1
+# enter the item:
+# Item2
+# enter the itemcode:
+# 02
+# enter 1 if you want to enter item or 0 if you 
+# dont want
+# 0
+# Item1 01
+# Item2 02
