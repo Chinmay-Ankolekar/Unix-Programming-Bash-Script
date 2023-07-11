6f637e186068d7a62a556366e030a2be5242f0cb
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-12 00:29:01 +0530

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
-while read -r line
+while read line
 do
     echo "$line" > tempo
-    v=$(awk '{ print $5 }' tempo)
-    emp=$(awk '{ print $1 }' tempo)
-
+    v=$(cut -d ' ' -f 5 tempo)
+    emp=$(cut -d ' ' -f 1 tempo)
     echo "$v"
 
-    if [ -n "$v" ] && [ "$v" -gt 0 ] 2>/dev/null; then
+    if echo "$v" | grep -qE '^[0-9]+$'; then
         if [ "$v" -gt 60000 ]; then
             da=$((v * 25 / 100))
         else
             da=$((v * 30 / 100))
         fi
-
         hra=$((v * 10 / 100))
         gs=$((v + da + hra))
 
         echo "$line"
         echo "Employee: $emp"
         echo "Basic Salary: $v"
-        echo "DA: $da"
-        echo "HRA: $hra"
-        echo "Gross Salary: $gs"    
+        echo "Gross Salary: $gs"
+    else
+        echo "Invalid value for Basic Salary: $v"
     fi
 done < empn.lst
-
 
 # output:-
 # 61000
