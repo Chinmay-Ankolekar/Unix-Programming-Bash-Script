d8cb14e485ec5bceee1585d65d3faafa9785fe21
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-11 22:43:24 +0530

    Update 11.sh

--- 11.sh
+++ 11.sh
@@ -1,26 +1,32 @@
-#! /bin/sh
+#!/bin/sh
 
-while read line
+while read -r line
 do
-echo $line > tempo
-v=$(cut -d ' ' -f 5 tempo)
-emp=$(cut -d ' ' -f 1 tempo)
-echo $v
-if [ $v -gt 60000 ]
-then
-da=$(($v * 25 / 100))
-else
-da=$(($v * 30 / 100))
-fi
-hra=$(($v * 10 / 100))
-gs=$(($v + da + hra))
-echo $line 
-echo "Employee: $emp"
-echo "Basic Salary : $v"
-echo "DA: $da"
-echo "HRA: $hra"
-echo "Gross Salary: $gs"
+    echo "$line" > tempo
+    v=$(awk '{ print $5 }' tempo)
+    emp=$(awk '{ print $1 }' tempo)
+
+    echo "$v"
+
+    if [ -n "$v" ] && [ "$v" -gt 0 ] 2>/dev/null; then
+        if [ "$v" -gt 60000 ]; then
+            da=$((v * 25 / 100))
+        else
+            da=$((v * 30 / 100))
+        fi
+
+        hra=$((v * 10 / 100))
+        gs=$((v + da + hra))
+
+        echo "$line"
+        echo "Employee: $emp"
+        echo "Basic Salary: $v"
+        echo "DA: $da"
+        echo "HRA: $hra"
+        echo "Gross Salary: $gs"    
+    fi
 done < empn.lst
+
 
 # output:-
 # 61000
