5f766c6d42e8c1928344b4690ea4b1e132b80007
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-28 21:45:26 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,5 +1,24 @@
-set `ls -l`
-echo $7 $3 $4 $0
+#!/bin/sh
+
+for file in *
+do
+    if [ -f $file ]
+    then
+        permission=$(stat -c '%A' "$file")
+        
+        link_existence="No"
+        if [ -h $file ]
+        then
+            link_existence="Yes"
+        fi
+        size=$(stat -c '%s' "$file")
+
+        Link_count=$(stat -c '%h' "$file")
+
+        echo "file: $file, permission: $permission, Link existence: $link_existence, size: $size bytes, link count: $Link_count"
+    fi
+done
+
 
 # Output:-
-# 620 -rwxr-xr-x 1 ./3.sh
+# file: 3.sh, permission: -rwxr-xr-x, Link existence: No, size: 442 bytes, link count: 1
