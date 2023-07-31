b2d834239867c3ed52b03911772ed3061530ce06
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-31 21:33:47 +0530

    Update 9.sh

--- 9.sh
+++ 9.sh
@@ -1,21 +1,23 @@
-#! /bin/sh
+#!/bin/sh
 
 if echo $1 | egrep -q '^[0-9]+$';
 then
-echo Valid code
+        echo "Valid Designation code"
 else
-echo Invalid code...Code must be numeric
-exit
+        echo "Invalid code, code must be numerical"
+        exit
 fi
-if echo $2 | egrep -q '^[a-zA-Z]+$' ;
+
+if echo $2 | egrep -q '^[a-zA-Z]+$';
 then
-echo Valid designation
-else
-echo Invalid code...Designation must contain alphabets
-exit
+        echo "Valid designation description"
+else 
+        echo "Invalid description , description must be alphabetical"
+        exit
 fi
+
 echo $1 $2 >> desig.lst
-echo Data added successfully
+echo "Data added successfully"
 cat desig.lst
 
 # Output:-
