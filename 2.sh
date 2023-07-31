07f04c0e1902d7fce4bdc36cd1c13102471d2454
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-07-31 18:57:58 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
 cd $1
-ls>list1.lst
+ls > list1.txt
 cd ..
+
 cd $2
-ls>list2.lst
+ls > list2.txt
 cd ..
 
 cd $1
-for word in `cat list1.lst`
+for word in `cat list1.txt`
 do
-cd ..
-cd $2
-grep "$word" "list2.lst"
-if [ $? -eq 0 ]
-then
-rm $word
-fi
-cd ..
-cd $1
+        cd ..
+        cd $2
+        grep $word "list2.txt"
+
+        if [ $? -eq 0 ]
+        then
+                rm $word
+        fi
+
+        cd ..
+        cd $1
 done
 
 # Output: -
