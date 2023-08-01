2dfa21011f747aac95943f76e4d2248f4b43f03f
Chinmay Ankolekar <122300321+Chinmay-Ankolekar@users.noreply.github.com>
2023-08-01 12:02:24 +0530

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
-echo Enter the filename
-read f
-echo Enter the pattern to be searched
-read pat
-if [ ! -f $f ]
+echo "Enter the filename"
+read filename
+
+if [ ! -f $filename ]
 then
-echo File does not exist
-exit
+        echo "File doesnot exists"
+        exit
 fi
-grep "$pat" $f
+
+echo "Enter the pattern to search"
+read pattern
+
+grep "$pattern" $filename
+
 if [ $? -eq 0 ]
 then
-echo Command executed successfully
+        echo "Command executed Successfully"
 else
-echo Command failed
+        echo "Command failed to execute"
 fi
 
 # Output:-
 # Enter the filename
 # pattern.txt
-# Enter the pattern to be searched
+# Enter the pattern to search
 # light
 # The sun is a powerful celestial body that illuminates our planet and provides us with warmth and light.
 # Sunlight is essential for the growth of plants through the process of photosynthesis.
